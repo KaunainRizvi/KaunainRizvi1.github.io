6401bd18317afc342658bbb2462ec3c975731872
KaunainRizvi <krizvibusiness.com>
2023-09-02 19:12:03 +0530

    Foliom

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -80,7 +80,7 @@ const Hero = () => {
 <div className="flex items-center">
   <div className="h-[1px] md:w-10 sm:w-7 vvs:w-5 w-4 bg-white"></div>
   <a
-    href="https://drive.google.com/file/d/1ML6yNdFlGDaRKRXVgoGma_JZ7yu4tm3z/view?usp=sharing"  
+    href="https://drive.google.com/file/d/1FQR3x6VjfIPqfiM4IeSaP_R3HINexBz8/view?usp=sharing"  
     download="Kaunain Rizvi's Resume"       
     target="_blank"                 
     className="text-white font-Glimer-Medium font-medium hover:text-[#EEA1BE] transition-colors duration-300 ease-in-out capitalize vss:text-sm md:text-base xl:text-lg text-xs"
